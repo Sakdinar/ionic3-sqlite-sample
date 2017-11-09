ce64bf5d054fb1428ea3034be08742a4eb5fe092
mp5er <michael.pfeiffer.dev@gmail.com>
2017-11-09 20:49:30 +0100

    cleanup database service

--- src/providers/database/database.service.ts
+++ src/providers/database/database.service.ts
@@ -1,12 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the DbProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DatabaseService {
 
